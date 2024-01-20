d5bb9ca019946d6482a1406606e5e67b4880035f
Renato <renamedrj@gmail.com>
2024-01-20 17:11:50 -0300

    Create folder script

---
+++ create-folder.ps1
@@ -0,0 +1,16 @@
+function Normalize-String($str) {
+    $normalizedString = $str -replace '[^\w\s]', '' -replace '\s', '-'
+    return $normalizedString.ToLower()
+}
+
+$classNumber = Read-Host "Enter the class number"
+$description = Read-Host "Enter the class description"
+
+$formattedClassNumber = "{0:D3}" -f [int]$classNumber
+
+$normalizedDescription = Normalize-String $description
+
+$folderName = "class-$formattedClassNumber-$normalizedDescription"
+New-Item -ItemType Directory -Path $folderName -Force
+
+Write-Host "Folder created successfully: $folderName"
