713dc3150d69c6316becb73c19e6a7e51db30846
Renato <renamedrj@gmail.com>
2024-01-20 18:07:42 -0300

    Updates finish class

--- finish-class.ps1
+++ finish-class.ps1
@@ -1,5 +1,14 @@
 $description = Read-Host "Enter the commit description"
 $commitMessage = "Class $description"
+
+$branchName = git rev-parse --abbrev-ref HEAD
+
 git add .
 git commit -m $description
 git push
+
+if ($branchName -ne "main") {
+    git checkout main
+    git pull --rebase origin $branchName
+}
+
