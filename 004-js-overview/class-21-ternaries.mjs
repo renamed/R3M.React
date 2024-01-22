515973e46e644160ba55e6ad80f6fca1d6eddd47
Renato <renamedrj@gmail.com>
2024-01-21 23:24:33 -0300

    Class 21. Ternaries Instead of if/else Statements

---
+++ 004-js-overview/class-21-ternaries.mjs
@@ -0,0 +1,12 @@
+import {getBook, getBooks, data} from './script.mjs'
+
+const book = getBook(2);
+
+const {pages, title, author, publicationDate, hasMovieAdaptation} = book;
+
+const summary = `${title}, a ${pages}-pages long book, was written by ${author} and published in ${publicationDate.split('-')[0]}. The book has ${hasMovieAdaptation ? "" : "not "}been adapted as a movie.`;
+console.log(summary)
+
+const pagesRange = pages > 1000 ? 'over a thousand' : 'less than a thousand';
+
+console.log(`The book has ${pagesRange}`);
