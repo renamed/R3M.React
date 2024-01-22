35715d4ab43a1961510db2b65e958fbf80ce5cea
Renato <renamedrj@gmail.com>
2024-01-21 22:43:29 -0300

    Class 18. Destructuring Objects and Arrays

---
+++ 004-js-overview/class-18-destruction-object-arrays.js
@@ -0,0 +1,15 @@
+import "./script.js"
+
+const books = getBooks();
+
+const book = getBook(2);
+
+// const title = book.title;
+// const author = book.author;
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } = book;
+console.log(author, title, genres);
+
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+const [primaryGenre, secondaryGenre] = genres;
+console.log(primaryGenre, secondaryGenre);
--- create-project.ps1
+++ create-project.ps1
@@ -23,6 +23,7 @@ $normalizedDescription = Format-String $description
 $projectName = "{0:D3}-{1}" -f $newNumber, $normalizedDescription
 
 Write-Host "=================================================="
+Write-Host "0. Empty"
 Write-Host "1. Create-React-App"
 Write-Host "2. Vite"
 Write-Host "3. Next.js"
@@ -31,8 +32,13 @@ Write-Host "--------------------------------------------------"
 do {
     $templateOption = Read-Host "Which should be use?"
     switch ($templateOption) {
+        "0" {
+            New-Item -ItemType Directory -Path $projectName -Force | Out-Null
+        }
         "1" {  
             npx create-react-app $projectName
+            Set-Location $projectName
+            npm run start
         }
         "2" {
             npm create vite@latest $projectName -- --template react 
@@ -42,14 +48,14 @@ do {
             npm run dev
         }
         "3" {
-
+            Write-Error "Not configured"
         }
         "4" {
-
+            Write-Error "Not configured"
         }
         Default {
-            Write-Error("Invalid options")
+            Write-Error "Invalid option"
         }
     }
-} while ($templateOption -notin "1", "2", "3", "4")
+} while ($templateOption -notin "0", "1", "2", "3", "4")
 Write-Host "=================================================="
