b3b9eaa5d77935d7e3b817c500ea0afd1b813a3e
Renato <renamedrj@gmail.com>
2024-01-22 21:18:37 -0300

    Class 31. Asynchronous JavaScript: Async/Await

---
+++ 004-js-overview/class-31-asynchronous-javascript-asyncawait.mjs
@@ -0,0 +1,13 @@
+async function getTodos() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data)
+
+    return data;
+}
+
+
+const todos = getTodos();
+
+console.log(todos);
+console.log('renato');
