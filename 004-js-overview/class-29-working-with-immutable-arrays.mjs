2dcc3fa0dfe46f6eab3483cba934d9996cec2e4f
Renato <renamedrj@gmail.com>
2024-01-22 21:06:15 -0300

    Class 29. Working With Immutable Arrays

---
+++ 004-js-overview/class-29-working-with-immutable-arrays.mjs
@@ -0,0 +1,21 @@
+import {getBook, getBooks} from './script.mjs'
+
+const books = getBooks();
+
+// 1) Add book to array
+const newBook = {
+    id: 6,
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J. K. Rowling'
+};
+
+const booksAfterAdd = [...books, newBook]
+console.log(booksAfterAdd)
+
+// 2) Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter(el => el.id !== 3);
+console.log(booksAfterDelete)
+
+// 3) Update book object in the array
+const booksAfterUpdate = booksAfterDelete.map(el => el.id == 1 ? {...el, pages: 1210 } : el)
+console.log(booksAfterUpdate)
